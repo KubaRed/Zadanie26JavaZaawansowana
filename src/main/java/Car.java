9c3b6c9e9591731da7ad5edf5a8e4f3eac807853
KUBA <kuba.piecuch.kp@gmail.com>
2022-12-08 20:25:03 +0100

    Task 1, task 2, task 3

---
+++ src/main/java/Car.java
@@ -0,0 +1,13 @@
+import com.sun.jdi.CharType;
+
+public class Car {
+    public String name;
+    public String description;
+    public CarType carType;
+
+    public Car(String name, String description, CarType carType) {
+        this.name = name;
+        this.description = description;
+        this.carType = carType;
+    }
+}
